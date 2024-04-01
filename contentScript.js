b5f5849026f6159482ecb9278ff93141724ca890
Chhavikant Mahobia <ckcodes907@gmail.com>
2024-04-01 22:13:39 +0530

    added manifest.json with assets

---
+++ contentScript.js
@@ -0,0 +1,12 @@
+(()=>{
+    let currentVideo="";
+
+    chrome.runtime.onMessage.addListener((obj, sender, response)=>{
+        const { type, value, videoId } = obj;
+
+        if(type === "NEW"){
+            currentVideo = videoId;
+            newVideoLoaded();
+        }
+    })
+})()
