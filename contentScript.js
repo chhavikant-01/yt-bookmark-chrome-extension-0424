c8113a767679e705c258b0c800df27aef06cecf7
Chhavikant Mahobia <ckcodes907@gmail.com>
2024-04-03 17:54:11 +0530

    appended bookmark button

--- contentScript.js
+++ contentScript.js
@@ -8,5 +8,44 @@
             currentVideo = videoId;
             newVideoLoaded();
         }
-    })
+    });
+
+    const newVideoLoaded = async ()=>{
+        const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
+        
+        if(!bookmarkBtnExists){
+            const bookmarkBtn = document.createElement("img");
+            bookmarkBtn.src = chrome.runtime.getURL("./assets/bookmark.png");
+            bookmarkBtn.className ="ytp-button "+"bookmark-btn";
+            bookmarkBtn.title = "Click to bookmark current timestamp";
+
+            youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
+            youtubePlayer = document.getElementsByClassName("video-stream")[0];
+            youtubeLeftControls.appendChild(bookmarkBtn);
+            bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
+        }
+
+        const addNewBookmarkEventHandler = ()=>{
+            const currentTime = youtubePlayer.currentTime;
+            const newBookmark = {
+                time: currentTime,
+                desc: "Bookmark at " + getTime(currentTime),
+            }
+        };
+        console.log(newBookmark);
+
+        chrome.storage.sync.set({
+            [currentVideo]: JSON.stringyfy([...curretVideoBookmarks, newBookmark].sort((a,b) => a.time - b.time))
+        })
+
+    }
+    newVideoLoaded();
+
 })()
+
+const getTime = t =>{
+    var date = new Date(0);
+    date.setSeconds(t);
+
+    return date.toISOString().substr(11,8);
+}
